1115d2db62861183edec8a68de9bd3e06669be7d
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 23:58:17 +0300

    Update 04 - Understanding distribution of categorical variables.py

--- Chapter 3 - Exploratory analysis in Python using Pandas/04 - Understanding distribution of categorical variables.py
+++ Chapter 3 - Exploratory analysis in Python using Pandas/04 - Understanding distribution of categorical variables.py
@@ -1,1 +1,7 @@
+# Training and Testing dataset are loaded in train and test dataframe respectively
 
+# Approved Loan in absolute numbers
+loan_approval = train['Loan_Status'].value_counts()['Y']
+
+# Two-way comparison: Credit History and Loan Status
+twowaytable = pd.crosstab(train ["Credit_History"], train ["Loan_Status"], margins=True)
