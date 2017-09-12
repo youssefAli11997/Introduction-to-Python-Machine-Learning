b85e2d8c3e56f507b4105dd7ca8901aa3ba5369e
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 21:14:47 +0300

    Create 05 - Why are conditional statements required?.py

---
+++ Chapter 2 - Python Libraries and data structures/05 - Why are conditional statements required?.py
@@ -0,0 +1,22 @@
+# Create a two integer variables a and b
+a=3
+b=4
+
+# if a is greater than b print a-b else a+b
+if a > b:
+    print (a-b)
+else:
+    print (a+b)
+
+# Create a list of squared numbers
+squares_list = [0, 1, 4, 9, 16, 25]
+
+# Store the length of squares_list in square_len
+square_len = len(squares_list)
+
+# if square_len is less than 5 then print "Less than 5" else "Greater than 5"
+if square_len < 5:
+    print ("Less than 5")
+else:
+    print ("Greater than 5")
+    
