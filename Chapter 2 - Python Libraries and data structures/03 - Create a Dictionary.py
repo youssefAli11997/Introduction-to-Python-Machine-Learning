f4124b708a7cd25a51360d02fdf1f754e6eb6302
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 21:09:48 +0300

    Create 03 - Create a Dictionary.py

---
+++ Chapter 2 - Python Libraries and data structures/03 - Create a Dictionary.py
@@ -0,0 +1,11 @@
+# Create a dictionary dict1
+dict1 = { 'Age': 16, 'Name': 'Max', 'Sports': 'Cricket'}
+
+# Update the value of Age to 18
+dict1['Age'] = 18
+
+# Print the value of Age
+print (dict1['Age'])
+
+# Store the keys of dictionary dict1 to dict_keys
+dict_keys = dict1.keys()
