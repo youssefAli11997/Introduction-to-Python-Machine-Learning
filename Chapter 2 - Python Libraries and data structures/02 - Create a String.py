8a58634aa43a251cce78683979161ea4517d775a
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 21:01:46 +0300

    Create 02 - Create a String.py

---
+++ Chapter 2 - Python Libraries and data structures/02 - Create a String.py
@@ -0,0 +1,17 @@
+# Create a string str1
+str1 = "Introduction with strings"
+
+# Now store the length of string str1 in variable str_len
+str_len = len(str1)
+
+str_new = "Machine Learning is awesome!"
+# Print last eight characters of string str_new (the length of str_new is 28 characters).
+print(str_new[-8:])
+#or
+#print(str_new[20:])
+
+str2 = "I am doing a course Introduction to Hackathon using "
+str3 = "Python"
+
+# Write a line of code to store concatenated string of str2 and str3 into variable str4
+str4 = str2 + str3
