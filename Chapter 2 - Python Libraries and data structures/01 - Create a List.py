481390fc3ecde176dfa09b55657db6363b95dce1
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 20:51:07 +0300

    Create 01 - Create a List.py

---
+++ Chapter 2 - Python Libraries and data structures/01 - Create a List.py
@@ -0,0 +1,11 @@
+# Create a list of squared numbers
+squares_list = [0, 1, 4, 9, 16, 25]
+
+# Now write a line of code to create a list of the first five odd numbers and store it in a variable odd_numbers
+odd_numbers = [1, 3, 5, 7, 9]
+
+# Print the first element of squares_list
+print (squares_list[0])
+
+# Print the second to fourth elements of squares_list
+print(squares_list[1:4])
